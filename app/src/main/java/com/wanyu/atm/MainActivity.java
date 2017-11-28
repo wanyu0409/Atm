c5d902842b2149444b8d9897305274a6c450677d
wanyu0409 <jasperwendy0409@gmail.com>
2017-11-28 11:10:38 +0800

    LoginActivity

--- app/src/main/java/com/wanyu/atm/MainActivity.java
+++ app/src/main/java/com/wanyu/atm/MainActivity.java
@@ -1,5 +1,6 @@
 package com.wanyu.atm;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,13 +11,17 @@ import android.view.Menu;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
-
+    boolean logon = false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        if (!logon){
+            Intent intent = new Intent(this, LoginActivity.class);
+            startActivity(intent);
+        }
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
