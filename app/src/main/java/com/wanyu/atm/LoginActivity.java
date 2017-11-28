4710354377fee108a9a1682f4893026ac3b5959b
wanyu0409 <jasperwendy0409@gmail.com>
2017-11-28 11:54:01 +0800

    login

--- app/src/main/java/com/wanyu/atm/LoginActivity.java
+++ app/src/main/java/com/wanyu/atm/LoginActivity.java
@@ -1,7 +1,11 @@
 package com.wanyu.atm;
 
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
 
@@ -9,5 +13,21 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+    }
+    public void login(View view){
+        EditText edUserid = (EditText) findViewById(R.id.userid);
+        EditText edPasswd = (EditText) findViewById(R.id.passwd);
+        String userid = edUserid.getText().toString();
+        String passwd = edPasswd.getText().toString();
+        if ("jack".equals(userid) && "1234".equals(passwd)){
+            Toast.makeText(this, "登入成功", Toast.LENGTH_LONG).show();
+            finish();
+        }else{
+            new AlertDialog.Builder(this)
+                    .setTitle("Atm")
+                    .setMessage("登入失敗")
+                    .setPositiveButton("OK", null)
+                    .show();
+        }
     }
 }
